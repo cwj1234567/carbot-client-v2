5bbeab6fa5376f3ac250c5766b3b712070945558
Craig <cwjordan@proton.me>
2023-01-08 23:10:14 -0500

    changed default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,70 @@
 /**
  * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
  */
+
 module.exports = {
+  // add the folders and files from your templates
   content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    "./content/**/*.html",
+    "./src/**/*.js",
     "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{ts,tsx}",
     "./public/**/*.html",
   ],
-  plugins: [
-    require("flowbite/plugin")
+
+  // make sure to safelist these classes when using purge
+  safelist: [
+    "w-64",
+    "w-1/2",
+    "rounded-l-lg",
+    "rounded-r-lg",
+    "bg-gray-200",
+    "grid-cols-4",
+    "grid-cols-7",
+    "h-6",
+    "leading-6",
+    "h-9",
+    "leading-9",
+    "shadow-lg",
   ],
-  theme: {},
+
+  // enable dark mode via class strategy
+  darkMode: "class",
+
+  theme: {
+    // colors: {
+    //   gray: colors.coolGray,
+    //   blue: colors.lightBlue,
+    //   red: colors.rose,
+    //   pink: colors.fuchsia,
+    // },
+
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [
+    // include Flowbite as a plugin in your Tailwind CSS project
+    require("flowbite/plugin"),
+  ],
 };
